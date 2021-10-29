b6096ccee3430d93181924eee5cacd2aeaad0a06
Justin Ennis <justin@nrc-llc.com>
2021-10-28 20:49:57 -0700

    Adding pre tags back in.

--- purpleair/purpleairdumpall.js
+++ purpleair/purpleairdumpall.js
@@ -11,7 +11,7 @@ function getDetails(sensorid, purpleAirApiReadKey) {
     fetch("https://api.purpleair.com/v1/sensors/"+sensorid, initObject)
     .then(response => response.json())
     .then(function (data) {
-        return JSON.stringify(data, null, 2);
+        return "<pre>"+JSON.stringify(data, null, 2) +"</pre>"
     })
     .catch(function (err) {
         console.log("ERROR: ", err);
