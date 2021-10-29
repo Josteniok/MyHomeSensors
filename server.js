8e728b2c8123aeea18c2e477ca54f44393406cb3
Justin Ennis <justin@nrc-llc.com>
2021-10-28 19:36:38 -0700

    Changed to use Express.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true });
+const express = require('express');
+const app = express();
 
 // PurpleAir API settings
 const purpleAirApiReadKey = process.env.API_READ_KEY || "";
@@ -9,23 +10,11 @@ const outdoorsensorid = process.env.OUTDOOR_SENSOR_ID || "";
 const indoorsensorindex = process.env.INDOOR_SENSOR_INDEX || "";
 const sensorgroupindex = process.env.SENSOR_GROUP_INDEX || "";
 
-import {getDetails} from './purpleair/purpleairdumpall';
-
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  // return { hello: process.env.TEST_VAR || 'hello' };
-  getDetails(indoorsensorindex, purpleAirApiReadKey);
+app.get('/', (req, res) => {
+  res.send('Hello, world')
 })
 
-
-
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-}
-start();
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
