50b2e3f93b4c393f804c05ae8648d82dc8677d17
Justin Ennis <justin@nrc-llc.com>
2021-10-28 20:18:15 -0700

    Attempting to connect to sensor.

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const purpleairdump = require('./purpleair/purpleairdumpall');
 
 // PurpleAir API settings
 const purpleAirApiReadKey = process.env.API_READ_KEY || "";
-const outdoorsensorid = process.env.OUTDOOR_SENSOR_ID || "";
+const outdoorsensorindex = process.env.OUTDOOR_SENSOR_ID || "";
 const indoorsensorindex = process.env.INDOOR_SENSOR_INDEX || "";
-const sensorgroupindex = process.env.SENSOR_GROUP_INDEX || "";
+const sensorgroupid = process.env.SENSOR_GROUP_ID || "";
 
 // Declare a route
 app.get('/', (req, res) => {
-  res.send('Hello, world')
+  // res.send('Hello, world')
+  res.send(purpleairdump.getDetails(indoorsensorindex, purpleAirApiReadKey))
 })
 
 const port = process.env.PORT || 3000;
