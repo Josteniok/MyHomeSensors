49469c1263d9b2abea935481283d86b5163c3264
Justin Ennis <justin@nrc-llc.com>
2021-11-25 15:18:06 -0800

    Testing the timestamp insertion.

--- server.js
+++ server.js
@@ -12,15 +12,31 @@ const outdoorsensorindex = process.env.OUTDOOR_SENSOR_ID || "";
 const indoorsensorindex = process.env.INDOOR_SENSOR_INDEX || "";
 const sensorgroupid = process.env.SENSOR_GROUP_ID || "";
 
+// Create a new pool for the database connection
+const {Pool} = require('pg');
+const pool = new Pool({
+ connectionString: "postgres://xguqgtzqeocepv:1d5abdafefd2742cd96ba650aee0857bbf9578764812aaf6b272e044941b8f46@ec2-18-214-214-252.compute-1.amazonaws.com:5432/davkg6fmjicq4k",
+ ssl: {
+ rejectUnauthorized: false
+ }
+});
+
+let currenttime = Date.now();
+// const timevalue = to_timestamp(currenttime.toString(), 'DD Mon YYYY');
+
+// Test writing a timestamp periodically to the test table
+pool.query('INSERT INTO TestTable(TimeStamp) VALUES($1)', [currenttime], (err, res) => {
+  if (err) {
+      console.log("Error - Failed to insert data into TestTable");
+      console.log(err);
+  }
+});
+
 // Declare a route
 app.get('/', (req, res) => {
   purpleairdump.getDetails(indoorsensorindex, purpleAirApiReadKey).then(response => {
-    console.log("About to print the data");
-    console.log("The data is %s.", response);
-    console.log("Try to output the JSON");
     res.json(response);
   });
-  // res.send('Hello, world');
 })
 
 const port = process.env.PORT || 3000;
