7994c56f56bb5b0c23fc9527c6c09b40f3da3453
Justin Ennis <justin@nrc-llc.com>
2022-12-25 20:26:05 -0800

    Moved purple air secrets setup into the purple air file.

--- purpleair/purpleairdumpall.js
+++ purpleair/purpleairdumpall.js
@@ -1,17 +1,22 @@
 'use strict';
 
-
 exports.getDetails = getDetails;
 
 const fetch = require('node-fetch');
 
-async function getDetails(sensorid, purpleAirApiReadKey) {
+// PurpleAir API settings
+const purpleAirApiReadKey = process.env.API_READ_KEY || "";
+const outdoorsensorindex = process.env.OUTDOOR_SENSOR_ID || "";
+const indoorsensorindex = process.env.INDOOR_SENSOR_INDEX || "";
+const sensorgroupid = process.env.SENSOR_GROUP_ID || "";
+
+async function getDetails() {
     let customHeader = new fetch.Headers();
     customHeader.append('X-API-Key', purpleAirApiReadKey);
     let initObject = {
         method: 'GET', headers: customHeader,
     };
-    const response = await fetch("https://api.purpleair.com/v1/sensors/"+sensorid, initObject, {});
+    const response = await fetch("https://api.purpleair.com/v1/sensors/"+indoorsensorindex, initObject, {});
     const json = await response.json();
 
     return json;
